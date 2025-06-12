de4bc41cf2b2d7770acea3ae6d658bed5f104162
Mathieu Marc Pobre <mathieu_marc_pobre@dlsu.edu.ph>
2025-06-12 15:11:02 +0800

    Main Menu Scene

---
+++ GDENG-GameJam1/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Kitchen");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
